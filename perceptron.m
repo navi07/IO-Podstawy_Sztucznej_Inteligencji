1566da294bd0cf4137c9429eb6968565977fdc8a
navi07 <kurzeja.patryk@gmail.com>
2017-10-18 00:00:20 +0200

    Sprawozdanie 1
    Budowa i dzialanie perceptronu

--- perceptron.m
+++ perceptron.m
@@ -6,10 +6,13 @@ A = [0 0 1 1];
 B = [0 1 0 1];
 P = [A;B];
 T = [0 0 0 1];
-net = init(net);
+%net = init(net);
+net.IW{1,1} = [8 9]; 
+net.b{1} = [10]
 Wyj_przed_treningiem = sim(net,P)
 net = train(net,P,T)
 Wyj_po_treningu = sim(net,P)
+
 
 %% OR
 clear all; close all; clc;
@@ -19,7 +22,9 @@ A = [0 0 1 1];
 B = [0 1 0 1];
 P = [A;B];
 T = [0 1 1 1];
-net = init(net);
+%net = init(net);
+net.IW{1,1}  = [8 8];
+net.b{1} = [9]
 Wyj_przed_treningiem = sim(net,P)
 net = train(net,P,T)
 Wyj_po_treningu = sim(net,P)
@@ -32,7 +37,9 @@ A = [0 0 1 1];
 B = [0 1 0 1];
 P = [A;B];
 T = [1 1 1 0];
-net = init(net);
+%net = init(net);
+net.IW{1,1}  = [7 7];
+net.b{1} = [10]
 Wyj_przed_treningiem = sim(net,P)
 net = train(net,P,T)
 Wyj_po_treningu = sim(net,P)
@@ -45,7 +52,9 @@ A = [0 0 1 1];
 B = [0 1 0 1];
 P = [A;B];
 T = [1 0 0 0];
-net = init(net);
+%net = init(net);
+net.IW{1,1}  = [9 9];
+net.b{1} = [8]
 Wyj_przed_treningiem = sim(net,P)
 net = train(net,P,T)
 Wyj_po_treningu = sim(net,P)
@@ -58,33 +67,10 @@ A = [0 0 1 1];
 B = [0 1 0 1];
 P = [A;B];
 T = [0 1 1 0];
-net = init(net);
+%net = init(net);
+net.IW{1,1}  = [7 7];
+net.b{1} = [1]
 Wyj_przed_treningiem = sim(net,P)
 net = train(net,P,T)
 Wyj_po_treningu = sim(net,P)
 
-%% XNOR
-clear all; close all; clc;
-
-net = newp([0 1; -2 2], 1);
-A = [0 0 1 1];
-B = [0 1 0 1];
-P = [A;B];
-T = [1 0 0 1];
-net = init(net);
-Wyj_przed_treningiem = sim(net,P)
-net = train(net,P,T)
-Wyj_po_treningu = sim(net,P)
-
-%% NOT
-clear all; close all; clc;
-
-net = newp([0 1; -2 2], 1);
-A = [0 1];
-B = [0 0 ];
-P = [A;B];
-T = [1 0 ];
-net = init(net);
-Wyj_przed_treningiem = sim(net,P)
-net = train(net,P,T)
-Wyj_po_treningu = sim(net,P)
