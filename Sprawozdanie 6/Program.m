9d623cf17c99ae28467cb992a02fd5b388d238d1
Patryk Kurzeja <kurzeja.patryk@gmail.com>
2017-12-29 00:38:06 +0100

    Sprawozdanie 6

---
+++ Sprawozdanie 6/Program.m
@@ -0,0 +1,40 @@
+close all; clear all; clc;
+
+           %A B C D E F G H I K L J M N O P R S T U
+INPUT =    [0 1 0 1 1 1 0 1 1 1 1 1 1 0 1 1 0 1 1 1;
+			1 1 1 1 1 1 1 0 0 1 0 0 0 1 1 1 1 1 0 0;
+			1 1 1 1 1 1 1 0 0 1 0 0 0 1 1 1 1 1 0 1;
+			0 0 1 0 1 1 1 1 0 1 1 0 1 0 0 0 1 0 1 0;%
+			1 1 1 1 1 1 1 1 1 0 1 1 1 1 1 1 1 0 1 1;
+			0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 1 0 0;
+			0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 1;
+			1 1 0 1 0 0 0 1 0 1 0 0 1 1 1 1 0 0 1 0;%
+			1 1 1 1 1 1 1 1 1 0 1 1 1 1 1 1 0 0 1 0;
+			1 1 0 0 1 1 0 1 0 0 1 0 0 0 1 1 1 1 0 1;
+			1 1 0 0 1 1 1 1 0 0 0 0 1 0 1 1 1 0 0 0;
+			1 0 0 1 0 0 1 1 0 1 0 0 1 1 0 0 0 0 1 0;%
+			1 1 1 1 1 1 1 1 1 0 1 1 1 1 1 1 0 0 1 0;
+			0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 1;
+			0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 1 0 0 0 0;
+			1 1 0 1 0 0 1 1 0 1 0 0 1 1 0 0 1 0 1 0;%
+			1 1 1 1 1 1 0 1 1 0 1 1 1 0 1 1 1 0 0 0;
+			0 1 1 1 1 0 1 0 0 0 0 1 0 1 0 0 1 1 1 1;
+			0 1 1 1 1 0 1 0 0 0 0 1 0 1 0 0 1 0 1 0;
+			1 0 1 0 1 0 1 1 0 1 1 1 1 0 0 1 0 0 0 0;%
+			];
+			
+% Parametry sieci Kohonena :
+dimensions = [4 5]; 
+coverSteps = 100;
+initNeighbor = 1;
+topologyFcn = 'gridtop';
+distanceFcn = 'dist';
+
+% Tworzenie SOM : 
+net = selforgmap(dimensions,coverSteps,initNeighbor,topologyFcn,distanceFcn);
+net.trainParam.epochs = 2000;
+
+% Trenowanie sieci : 
+[net,tr] = train(net,INPUT);
+y = net(INPUT);
+classes = vec2ind(y);
